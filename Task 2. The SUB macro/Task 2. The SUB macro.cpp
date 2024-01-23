92ef979ae57f5117d2a11d50fffcc386114ddbf2
Knyazev Nikita <nk8222138@gmail.com>
2024-01-24 02:15:57 +0300

    Добавьте файлы проекта.

---
+++ Task 2. The SUB macro/Task 2. The SUB macro.cpp
@@ -0,0 +1,22 @@
+﻿// Task 2. The SUB macro.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+#define SUB(a,b) ((a)-(b)) 
+
+#ifndef SUB
+
+#error SUB Does not exists
+
+#endif
+
+int main(int argc, char** argv)
+{
+	int a = 6;
+	int b = 5;
+	int c = 2;
+	std::cout << SUB(a, b) << std::endl;
+	std::cout << SUB(a, b) * c << std::endl;
+	std::cout << SUB(a, b + c) * c << std::endl;
+}
